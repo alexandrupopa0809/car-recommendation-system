2cd5ab366ee4a5be78363fef3540703a0e49a78c
Alexandru Popa <alex9popa@yahoo.com>
2024-01-06 17:15:14 +0200

    Add pages

--- rec_system/main_app/urls.py
+++ rec_system/main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('filters/', views.filters, name='filters'),
+    path('recommendations/', views.process_filters, name='recommendations'),
 ]
--- rec_system/main_app/views.py
+++ rec_system/main_app/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render
 
 
 def home(request):
+    team_name = "Auto Advise Team"
+    team_members = ["Popa Alexandru-Matei", "Varzaru Mihnea-Gabriel"]
+    return render(request, 'home.html', {'team_name': team_name, 'team_members': team_members})
+
+
+def filters(request):
+    return render(request, "filters.html")
+
+
+def process_filters(request):
     if request.method == "POST":
-        user_input = request.POST.get("user_input", "")
-        print(f"User input: {user_input}")
-    return render(request, "home.html")
+        mileage = request.POST.get("mileage")
+        price = request.POST.get("price")
+        horsepower = request.POST.get("horsepower")
+        description = request.POST.get("description")
+
+        results = {
+            "mileage": mileage,
+            "price": price,
+            "horsepower": horsepower,
+            "description": description,
+        }
+
+        return render(request, "recommendations.html", results)
+
+    return render(request, "filters.html")
